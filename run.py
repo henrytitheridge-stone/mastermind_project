c3c5bc2154b4877da1d2f6f13d209fe75fb9a270
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2025-08-16 14:56:53 +0100

    Add inital functions structure to run.py file

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def generate_secret_code():
+
+
+def display_board():
+
+
+def validate_player_input():
+
+
+def show_feedback():
+
+
+def play_mastermind():
