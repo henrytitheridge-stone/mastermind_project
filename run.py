4ba973e78d30fb68970fe37408672aa2c291d81c
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2025-08-16 16:35:24 +0100

    Add separating '|' lines to board and make titles centered

--- run.py
+++ run.py
@@ -11,19 +11,26 @@ class Board:
         pass
 
     def display_current_board(self, guess_pegs, feedback_pegs):
+
         line = "------------------------------------------"
+        instructions_title = "Instructions"
+        game_title = "MASTERMIND"
+
         print(line)
-        print("Instructions")
+        print(instructions_title.center(42))
         print(line)
+
         print("Enter these numbers to guess the secret code:")
         print("1 = RED, 2 = ORANGE, 3 = YELLOW, 4 = GREEN, 5 = BLUE, 6 = PURPLE")
         print("W = a peg was guessed in the correct colour")
         print("B = a peg was guessed in the correct colour AND position")
+
         print(line)
-        print("MASTERMIND")
+        print()
+        print(game_title.center(42))
         print(line)
 
-        print("        ", end="")
+        print("      |   ", end="")
         for x in secret_code:
             print(x, end="     ")
 
@@ -31,8 +38,8 @@ class Board:
 
         for i in range(8):
             print(line)
-            print(feedback_pegs[i][0], feedback_pegs[i][1])
-            print(feedback_pegs[i][2], feedback_pegs[i][3], end="     ")
+            print("|", feedback_pegs[i][0], feedback_pegs[i][1], "|")
+            print("|", feedback_pegs[i][2], feedback_pegs[i][3], end=" |   ")
 
             for x in guess_pegs[i]:
                 print(x, end="     ")
@@ -51,7 +58,7 @@ class Board:
 
 
 colours = ["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "PURPLE"]
-guess_pegs = [["o", "o", "o", "o"] for _ in range(8)]
+guess_pegs = [[" o ", " o ", " o ", " o "] for _ in range(8)]
 feedback_pegs = [[".", ".", ".", "."] for _ in range(8)]
 secret_code = generate_secret_code(colours)
 print(secret_code)
